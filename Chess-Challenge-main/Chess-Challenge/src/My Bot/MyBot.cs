83a11fbeee1e803aefadf04524d68c9749d775f2
dylan1225 <123887911+dylan1225@users.noreply.github.com>
2024-07-23 10:38:24 -0400

    Update MyBot.cs

--- Chess-Challenge-main/Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge-main/Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,19 @@
-﻿using ChessChallenge.API;
+using ChessChallenge.API;
 
 public class MyBot : IChessBot
 {
+	// chess piece values for evaluation, empty, pawn, bishop, horse, rook, queen, king 
+
+	int[] piecevalue = {0, 100, 350, 300, 900, 1000000};
+
+	public Move MinMax(Board board, int depth = 0, int max, int min){
+		
+	}
+
     public Move Think(Board board, Timer timer)
     {
-        Move[] moves = board.GetLegalMoves();
+        Move[] legal_move = board.GetLegalMoves();
+		MinMax(board)
         return moves[0];
     }
 }
